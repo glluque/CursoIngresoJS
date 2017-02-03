d1b95ecbf9ad7c5b3b81c8db2a8bf056049a93ca
Guillermo Luque <gl.luque@hotmail.com>
2017-02-03 12:59:41 -0300

    4to commint
    ejercicio e/s 7

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,52 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+	var numeroUno;
+	var numeroDos;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	var resultado=numeroUno+numeroDos;
+	alert("La suma es " +resultado);
+}
+
+function restar()
+{
+	var numeroUno;
+	var numeroDos;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	var resultado=numeroUno-numeroDos;
+	alert("La resta es " +resultado);
+}
+
+function multiplicar()
+{ 
+	var numeroUno;
+	var numeroDos;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	var resultado=numeroUno*numeroDos;
+	alert("La multiplicación es " +resultado);
+}
+
+function dividir()
+{
+	var numeroUno;
+	var numeroDos;
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	var resultado=numeroUno/numeroDos;
+	alert("La división es " +resultado);
+}
+
