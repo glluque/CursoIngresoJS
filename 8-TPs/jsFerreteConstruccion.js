4052c8aa27cb62d63035eac6fbf6b6a23b52e8f6
Guillermo Luque <gl.luque@hotmail.com>
2017-02-09 13:16:19 -0300

    Ejercicios TP 2 y 3
    En colaboración con el usuario HildalgoS2207

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,7 +6,15 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-	var terrenoLargo;
+	var largo=document.getElementById('Largo').value;
+	var ancho=document.getElementById('Ancho').value;
+	var largoAlambre=3*((parseInt(largo)*2)+(parseInt(ancho)*2));
+	alert("Son requeridos "+largoAlambre+" metros de alambre.");
+	//parseFloat tirara numeros con coma, en lugar de paseInt que tira numeros enteros
+	
+	//Formulado en parte al que hizo el usuario HidalgoS2207
+
+	/*var terrenoLargo;
 	var terrenoAncho;
 	//var terrenoRadio;
 	terrenoLargo=document.getElementById('Largo').value;
@@ -19,21 +27,34 @@ function Rectangulo ()
 	var alambreTres=perimetro*3;
 	alert("Son requeridos "+alambreTres+" metros de alambre");
 	//NaN en js es Not a number
-	//el NaN era porque estaba indicando el parseInt "Largo" en lugar de terrenoLargo, lo mismo con terrenoAncho
+	//el NaN era porque estaba indicando el parseInt "Largo" en lugar de terrenoLargo, lo mismo con terrenoAncho*/
 
 }
 function Circulo () 
 {
-	var terrenoRadio;
+	var radio=document.getElementById('Radio').value;
+	var largoAlambre=3*(parseInt(radio)*parseInt(radio)*3.14);
+	alert("Son requeridos "+largoAlambre+" metros de alambre.");
+
+	//Formulado en parte al que hizo el usuario HidalgoS2207
+
+	/*var terrenoRadio;
 	terrenoRadio=document.getElementById('Radio').value;
 	terrenoRadio=parseInt(terrenoRadio);
 	var diametro=terrenoRadio*terrenoRadio;
 	var circunferencia= diametro*3.14;
 	alert("Son requeridos "+circunferencia+" metros de alambre");
-	//no olvidar poner + para concatenar
+	//no olvidar poner + para concatenar*/
 
 }
 function Materiales () 
 {
-	
+	var largo=document.getElementById('Largo').value;
+	var ancho=document.getElementById('Ancho').value;
+	var metrosCuadrados=(parseInt(largo))*(parseInt(ancho));
+	var cemento=metrosCuadrados*2;
+	var cal=metrosCuadrados*3;
+	alert("La cantidad a utilizar es "+cemento+" bolsas de cemento y "+cal+" de cal.")
+
+	//Formulado en parte al que hizo el usuario HidalgoS2207
 }
