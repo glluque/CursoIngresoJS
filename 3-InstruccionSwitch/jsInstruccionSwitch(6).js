fba97485464ba90fa1589b72f51b583219f9c16e
Guillermo Luque <gl.luque@hotmail.com>
2017-02-14 14:36:06 -0300

    Switch terminanda

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -23,8 +23,22 @@ function Mostrar()
 		case 19:
 			alert("Es de tarde");
 			break;
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+		case 24:
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			alert("Es de noche");
+			break;
 	default:
-		alert("Es de noche");
+		alert("La hora no existe");
 		break;
 	}
 
