760e39fff054d4975d03f7519cc00a20fd59f045
Guillermo Luque <gl.luque@hotmail.com>
2017-02-14 14:32:50 -0300

    Switch 5 y 6
    Switch terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,17 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
-laHora=parseInt(laHora);
-swith(laHora) {
-	case 7:
-	case 8:
-	case 9:
-	case "10":
-	case "11":
-		alert("Es de mañana");
-		break;
-}
+	var laHora = document.getElementById('hora').value;
+	laHora=parseInt(laHora);
+	
+	switch(laHora)
+	{
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("Es de Mañana");
+			break;
+		default:
+			alert("La hora es "+laHora);
+			break;
+	}
 
 //alert (laHora);
 	
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -1,7 +1,32 @@
 function Mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+	var laHora = document.getElementById('hora').value;
+	laHora=parseInt(laHora);
+	
+	switch(laHora)
+	{
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("Es de Mañana");
+			break;
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+			alert("Es de tarde");
+			break;
+	default:
+		alert("Es de noche");
+		break;
+	}
 
 
 
