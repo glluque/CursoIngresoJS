5c2a301343b81565ef821c6c67e0b780cd8664da
Guillermo Luque <gl.luque@hotmail.com>
2017-02-17 14:36:10 -0300

    TP 12

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,81 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+ 	var edad=Number.MAX_VALUE;
+    var sexo="Undefined"; //preguntar
+    var estadoCivil=Number.MAX_VALUE; 
+    var sueldoBruto=Number.MIN_VALUE; 
+    var numerolegajo=Number.MAX_VALUE; 
+    var nacionalidad="Undefined";
+
+    while(edad > 90 || edad < 18 || isNaN(edad))//preguntar
+    {
+        edad = prompt("Ingrese edad de la persona: " , "Entre 18 y 90 años");
+        edad = parseInt(edad);
+
+        if(edad > 90 || edad < 18 || isNaN(edad))
+        {
+            alert("Por favor reingrese edad de la persona.");
+        }
+    }
+
+    while(sexo != "M" && sexo != "F")
+    {
+        sexo = prompt("Ingrese sexo de la persona: " , "M para masculino ó F para femenino");
+
+        if(sexo != "M" && sexo != "F")
+        {
+            alert("Ingrese sexo de la persona nuevamente.");
+        }
+    }
+
+    while(estadoCivil < 1 || estadoCivil > 4 || isNaN(estadoCivil))
+    {
+        estadoCivil = prompt("Ingrese Estado Civil de la persona: " , "1 para Soltero | 2 para Casado | 3 para Divorciados | 4 para Viudos");
+        estadoCivil = parseInt(estadoCivil);
+
+        if(estadoCivil > 4 || estadoCivil < 1 || isNaN(estadoCivil))
+        {
+            alert("Ingrese estado civil de la persona nuevamente.");
+        }
+    }
+
+    while(sueldoBruto < 8000 || isNaN(sueldoBruto))
+    {
+        sueldoBruto = prompt("Ingrese sueldo bruto de la persona: " , "Mayor a $8000 sin el signo $");
+        sueldoBruto = parseInt(sueldoBruto);
+
+        if(sueldoBruto < 8000 || isNaN(sueldoBruto))
+        {
+            alert("Sueldo inferior a $8000 no válido. Ingrese sueldo bruto de la persona nuevamente.");
+        }
+    }
+
+    while(numerolegajo > 9999 || numerolegajo < 1000 || isNaN(numerolegajo))
+    {
+        numerolegajo = prompt("Ingrese número de legajo de la persona: " , "XXXX");
+        numerolegajo = parseInt(numerolegajo);
+
+        if(numerolegajo > 9999 || numerolegajo < 1000 || isNaN(numerolegajo))
+        {
+            alert("Ingrese número de legajo de la persona nuevamente.");
+        }
+    }
+
+    while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N")
+    {
+        nacionalidad = prompt("Ingrese nacionalidad de la persona: " , "A para Argentina | E para Extranjeros | N para Nacionalizados");
+
+        if(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N")
+        {
+            alert("Ingrese nacionalidad de la persona nuevamente.");
+        }
+    }
+    
+    document.getElementById('Edad').value = edad;
+    document.getElementById('Sexo').value = sexo;
+    document.getElementById('EstadoCivil').value = estadoCivil;
+    document.getElementById('Sueldo').value = sueldoBruto;
+    document.getElementById('Legajo').value = numerolegajo;
+    document.getElementById('Nacionalidad').value = nacionalidad;
 }
