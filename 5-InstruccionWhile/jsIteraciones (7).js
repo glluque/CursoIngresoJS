3262174098ec06401abd41364128eb4dcb02b8a5
Guillermo Luque <gl.luque@hotmail.com>
2017-02-16 22:05:06 -0300

    while 7 cambios

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,9 +4,25 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	var promedio;
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	while(respuesta=='si')
+	{
+		contador++;
+		numero=prompt("Ingrese un número");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+
+		respuesta=prompt("Ingrese 'si' para finalizar");
+	}
+
+	promedio=acumulador/contador;
+
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
+
+//para salir del while en este se usa resp=="s"
